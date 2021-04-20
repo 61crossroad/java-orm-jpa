99398ee2d38b1e2dcd6238f3a3ea0e8712e86a75
김승환 <kimsh@itsdcode.com>
2021-04-21 01:44:54 +0900

    EntityManagerFactory executes ddl-auto

--- MemberTeamRelation/src/main/java/org/jetlag/MemberTeamRelation.java
+++ MemberTeamRelation/src/main/java/org/jetlag/MemberTeamRelation.java
@@ -1,7 +1,14 @@
 package org.jetlag;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class MemberTeamRelation {
     public static void main(String[] args) {
-        System.out.println("Member and Team Relation");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("member_team");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
     }
 }
