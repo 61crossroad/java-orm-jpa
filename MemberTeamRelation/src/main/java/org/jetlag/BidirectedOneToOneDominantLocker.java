639408b549cbb8993f0912661d8b5edc228886e2
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-21 23:46:18 +0900

    bidirected dominant 1:1

---
+++ MemberTeamRelation/src/main/java/org/jetlag/BidirectedOneToOneDominantLocker.java
@@ -0,0 +1,14 @@
+package org.jetlag;
+
+import javax.persistence.*;
+
+@Entity
+public class BidirectedOneToOneDominantLocker {
+    @Id @GeneratedValue @Column(name = "LOCKER_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private BidirectedOneToOneDominantMember bidirectedOneToOneDominantMember;
+}
---
+++ MemberTeamRelation/src/main/java/org/jetlag/BidirectedOneToOneDominantMember.java
@@ -0,0 +1,14 @@
+package org.jetlag;
+
+import javax.persistence.*;
+
+@Entity
+public class BidirectedOneToOneDominantMember {
+    @Id @GeneratedValue @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String username;
+
+    @OneToOne @JoinColumn(name = "LOCKER_ID")
+    private BidirectedOneToOneDominantLocker bidirectedOneToOneDominantLocker;
+}
