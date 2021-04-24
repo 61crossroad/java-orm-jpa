ad81e4d6d3e1d82fd1be20aa3f0d25b6d82b5e7e
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-24 14:50:36 +0900

    MappedSuperclass

---
+++ AdvancedMappings/src/main/java/org/jetlag/BaseMember.java
@@ -0,0 +1,8 @@
+package org.jetlag;
+
+import javax.persistence.Entity;
+
+@Entity
+public class BaseMember extends BaseEntity {
+    private String email;
+}
---
+++ AdvancedMappings/src/main/java/org/jetlag/BaseSeller.java
@@ -0,0 +1,8 @@
+package org.jetlag;
+
+import javax.persistence.Entity;
+
+@Entity
+public class BaseSeller extends BaseEntity {
+    private String shopName;
+}
