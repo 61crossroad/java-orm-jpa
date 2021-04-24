be80e302aff6eec508487b4e290059f0bb604e04
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-24 15:59:49 +0900

    embeddable types in entity

---
+++ ValueTypes/src/main/java/org/jetlag/Address.java
@@ -0,0 +1,12 @@
+package org.jetlag;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    @Column(name = "city")
+    private String city;
+    private String street;
+    private String zipcode;
+}
