00d4f3f22b31565a115134e64efb5fc9229a2934
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-25 02:08:51 +0900

    Address is Value Object

--- JPQL-Criteria-QueryDSL-NativeSQL/src/main/java/org/jetlag/entity/Address.java
+++ JPQL-Criteria-QueryDSL-NativeSQL/src/main/java/org/jetlag/entity/Address.java
@@ -1,12 +1,14 @@
 package org.jetlag.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 @Embeddable
 public class Address {
     private String city;
