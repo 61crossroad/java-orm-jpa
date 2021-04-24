51901ad3f82b013fb8a16046b3ae51892ceafbae
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-24 14:52:28 +0900

    MappedSuperclass - AttributeOverride(s)

--- AdvancedMappings/src/main/java/org/jetlag/BaseMember.java
+++ AdvancedMappings/src/main/java/org/jetlag/BaseMember.java
@@ -1,7 +1,13 @@
 package org.jetlag;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-
+@AttributeOverrides({
+        @AttributeOverride(name = "id", column = @Column(name = "MEMBER_ID")),
+        @AttributeOverride(name = "name", column = @Column(name = "MEMBER_NAME"))
+})
 @Entity
 public class BaseMember extends BaseEntity {
     private String email;
--- AdvancedMappings/src/main/java/org/jetlag/BaseSeller.java
+++ AdvancedMappings/src/main/java/org/jetlag/BaseSeller.java
@@ -1,7 +1,10 @@
 package org.jetlag;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
+@AttributeOverride(name = "ID", column = @Column(name = "SELLERID"))
 @Entity
 public class BaseSeller extends BaseEntity {
     private String shopName;
