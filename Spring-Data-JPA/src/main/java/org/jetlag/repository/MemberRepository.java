f7f3c75031dfd576fcc121b1c342716082b59495
iMac21.5-2012 <61crossroad@gmail.com>
2021-04-30 22:16:56 +0900

    jpql query & native query

--- Spring-Data-JPA/src/main/java/org/jetlag/repository/MemberRepository.java
+++ Spring-Data-JPA/src/main/java/org/jetlag/repository/MemberRepository.java
@@ -2,9 +2,19 @@ package org.jetlag.repository;
 
 import org.jetlag.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByEmailAndName(String email, String name);
+
+    List<Member> findByName(@Param("name") String name);
+
+    @Query("select m from Member m where m.name = ?1")
+    Member findByNameQuery(String name);
+
+    @Query(value = "SELECT * FROM member WHERE name = 0?", nativeQuery = true)
+    Member findByNameNative(String name);
 }
---
+++ Spring-Data-JPA/src/main/java/org/jetlag/repository/MemberRepositoryImpl.java
@@ -0,0 +1,20 @@
+package org.jetlag.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.jetlag.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class MemberRepositoryImpl {
+    private final EntityManager em;
+
+    List<Member> findByName(String name) {
+        return em.createNamedQuery("Member.findByName", Member.class)
+                .setParameter("name", "user1")
+                .getResultList();
+    }
+}
